8b5018543063c2653360a42cd2d10aaecca7ace3
task11 <task11@naver.com>
2021-11-14 13:13:56 +0900

    react-component add

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
-import {Component} from 'react'
-import logo from './logo.svg';
+import React, {Component} from 'react'
 import './App.css';
+
+class Subject extends Component {
+  render () {
+    return (
+      <header>
+        <h1>{this.props.title}</h1>
+        {this.props.sub}
+      </header>
+    );
+  }
+}
+
+class TOC extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li><a href="1.html">HTML</a></li>
+          <li><a href="2.html">CSS</a></li>
+          <li><a href="3.html">JavaScript</a></li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+class Content extends Component {
+  render () {
+    return (
+      <article>
+        <h2>{this.props.title}</h2>
+        {this.props.desc}
+      </article>
+    );
+  }
+}
 
 class App extends Component {
   render () {
     return (
       <div className="App">
-      
-        <h1>Do whatever you can.</h1>
-          
+        <Subject title="WEB" sub="World Wide Web!"></Subject>
+        <Subject title="React" sub="For U.I"></Subject>
+        <TOC></TOC>
+        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
     );
   }
